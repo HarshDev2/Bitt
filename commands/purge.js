7fdcb2ad8fbc6dc2f46f4c3691b15834714c5854
harshdev2 <averageworkspace@gmail.com>
2022-10-25 11:04:29 +0530

    Updated Embeds

--- commands/purge.js
+++ commands/purge.js
@@ -3,10 +3,19 @@ const { EmbedBuilder, PermissionsBitField } = require('discord.js');
 exports.run = (client, message, args) => {
     const amount = parseInt(args[0]);
 
-    if(amount > 100 || amount < 1) return message.channel.send('Enter an amount between 1 and 100.');
+    const AmountError = new EmbedBuilder()
+    .setDescription('Enter an amount between 1 and 100.')
+
+    const AmountDeleted = new EmbedBuilder()
+    .setDescription(`Sucessfully deleted ${amount} messages.`)
+
+    if(amount > 100 || amount < 1) {
+        message.channel.send({ embeds: [AmountError], ephemeral: true})
+    }
+    
     else { 
         message.channel.bulkDelete(amount);
-        message.channel.send(`Sucessfully deleted ${amount} messages.`);
+        message.channel.send({ embeds: [AmountDeleted] });
     }
 }
 exports.name = 'purge'
