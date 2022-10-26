e0d17b6837972d0c7f7798cb315ff06cb4b5e21e
harshdev2 <averageworkspace@gmail.com>
2022-10-26 09:41:31 +0530

    Made Quiz Command Little Bit

--- slash_commands/quiz.js
+++ slash_commands/quiz.js
@@ -3,8 +3,32 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('quiz')
-		.setDescription('Starts a Quiz!'),
+		.setDescription('Starts a Quiz!')
+        .addStringOption(option =>
+            option.setName('category')
+                .setDescription('Category of questions.')
+                .setRequired(true)
+                .addChoices(
+                    { name: 'History', value: 'History' },
+                    { name: 'Arts', value: 'Arts' },
+                    { name: 'Comics', value: 'Comics' },
+                    { name: 'Computer', value: 'Computer' },
+                    { name: 'Animals', value: 'Animals' },
+                    { name: 'Films', value: 'Movies' },
+                ))
+        .addStringOption(option =>
+            option.setName('difficulty')
+                .setDescription('The difficulty of questions.')
+                .addChoices(
+                    { name: 'Easy', value: 'Easy' },
+                    { name: 'Medium', value: 'Medium' },
+                    { name: 'Hard', value: 'Hard' },
+                )),
+
 	async execute(interaction) {
-		await interaction.reply('Quiz is being built.');
+        const category = interaction.options.getString('category');
+        const difficulty = interaction.options.getString('difficulty');
+
+		await interaction.reply(`You choosed ${category} Category and ${difficulty} Difficulty. \n \n Quiz is being built.`);
 	},
 };
