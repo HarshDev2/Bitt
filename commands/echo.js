946aa0cf2a8eec4d3f7d231a70140c90fd4c22c7
harshdev2 <averageworkspace@gmail.com>
2022-10-28 11:26:48 +0530

    Added Echo Command

---
+++ commands/echo.js
@@ -0,0 +1,22 @@
+const { SlashCommandBuilder, PermissionsBitField, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('echo')
+		.setDescription('make the bot say anything!')
+        .addStringOption(option =>
+            option.setName('message')
+                .setDescription('message that bot will say!')
+                .setRequired(true)),
+
+	async execute(interaction) {
+        await interaction.deferReply({ephemeral: true})
+        const message = interaction.options.getString('message');
+        const messageEmbed = new EmbedBuilder()
+	    .setDescription(message)
+
+        await interaction.channel.send({embeds: [messageEmbed]});
+        await interaction.editReply({content: 'Message Sent!'});
+
+	},
+};
