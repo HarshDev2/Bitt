48b8cc088d490c90526d360cb83ca59b50698e6d
harshdev2 <averageworkspace@gmail.com>
2022-10-28 08:16:49 +0530

    =

---
+++ commands/kick.js
@@ -0,0 +1,22 @@
+const { SlashCommandBuilder, PermissionsBitField } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('purge')
+		.setDescription('Purge any amount of messages!')
+        .addIntegerOption(option =>
+            option.setName('amount')
+                .setDescription('Amount of message to be cleared.')
+                .setRequired(true))
+        .setDefaultMemberPermissions(PermissionsBitField.Flags.ManageMessages),
+
+	async execute(interaction) {
+        const amount = interaction.options.getInteger('amount');
+
+         await interaction.channel.bulkDelete(amount);
+
+         await interaction.reply(`Sucessfully Deleted ${amount} messages.`);
+         setTimeout(() => interaction.deleteReply(), 3000);
+
+	},
+};
