171b753dd5068e9f370d10f8c4512796e4f5b279
Harsh Agrawal <averageworkspace@gmail.com>
2022-10-31 09:07:59 +0530

    updated help command

--- commands/help.js
+++ commands/help.js
@@ -1,16 +1,17 @@
-const { SlashCommandBuilder, PermissionsBitField, EmbedBuilder } = require('discord.js');
+const { SlashCommandBuilder, PermissionsBitField, EmbedBuilder, ActionRowBuilder, ButtonBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('help')
+		.setName('helpt')
 		.setDescription('shows all the commands bot have'),
 
 	async execute(interaction) {
+
         const HelpEmbed = new EmbedBuilder()
         .setColor(0x000000)
 	    .setTitle('Bot Help!')
-	    .setDescription('I use slash commands \n \n **Bot Commands -** \n \n help - shows this help page. \n \n purge - clears any amount of messages you want. \n \n ping - replies with pong message \n \n quiz - starts a quiz. \n \n info - shows bot info')
-	    .setThumbnail('https://styles.redditmedia.com/t5_3b1wr/styles/communityIcon_qdbg6bz0bud71.png?width=256&s=ccf3d06bf3b8056f312f207c7ce906cf69af6efd')
+	    .setDescription('I use slash commands. \n \n **Bot Commands -** \n \n **Utility Commands ** \n \n ** /help ** \nshows this help page. \n \n ** info ** \n  shows bot info. \n \n ** /announce ** \n announces a messge in embed or without embed in a specified channel. \n \n ** /purge ** \nclears any amount of messages you want.\n \n ** /role create ** \n Adds a role to the server. \n \n **/role delete ** \n Deletes a role from the server. \n \n ** /ban ** \n bans a user from the server. \n \n ** /unban ** \n unbans a user from the server. \n \n ** /nickname change ** \n changes the nickname of a user. \n \n ** Fun Commands ** \n \n ** /pfp ** \n shows a user profile picture. \n \n ** /ping ** \n replies with pong message. \n \n ** /gay ** \n shows gayrate of a user. \n \n ** /echo ** \n make the bot say whatever you want.')
+		.setThumbnail('https://cdn.discordapp.com/avatars/1032836183065116722/b2628cef1a5640df7aac021d7e927b68.webp')
 	    .setTimestamp()
    
          await interaction.reply({embeds: [HelpEmbed]});
