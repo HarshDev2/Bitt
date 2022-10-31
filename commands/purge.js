3dd539a85ad73c35df6743a5177fe2c1d890e417
Harsh Agrawal <averageworkspace@gmail.com>
2022-10-31 09:27:44 +0530

    updated purge command

--- commands/purge.js
+++ commands/purge.js
@@ -12,11 +12,13 @@ module.exports = {
 	async execute(interaction) {
         const amount = interaction.options.getInteger('amount');
 
-        if(!interaction.member.permissions.has(PermissionsBitField.Flags.ManageMessages)) return interaction.reply({content: 'You do not have permission to use this command'});
+        if(!interaction.member.permissions.has(PermissionsBitField.Flags.ManageMessages)) return interaction.reply({content: 'You do not have permission to use this command', ephemeral: true});
+
+        else if(amount < 1 || amount > 100) return interaction.reply({content: 'Amount must be greater than 0 and less than 100', ephemeral: true});
 
         else if(interaction.member.permissions.has(PermissionsBitField.Flags.ManageMessages)) {
             interaction.channel.bulkDelete(amount);
-            interaction.reply(`Sucessfully Deleted ${amount} messages.`);
+            interaction.reply({content: `Sucessfully Deleted ${amount} messages.`});
             setTimeout(() => interaction.deleteReply(), 3000);
         }
 
