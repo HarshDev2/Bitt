deece8bcfeac6ffb41f1663683a1cef0b6c2e3e8
NehaDevs <cuteybg@gmail.com>
2022-11-03 04:40:29 +0000

    updated user command

--- commands/user.js
+++ commands/user.js
@@ -2,9 +2,22 @@ const { SlashCommandBuilder, PermissionsBitField, EmbedBuilder } = require('disc
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('user')
-		.setDescription('Provides information about the user.'),
+		.setName('usert')
+		.setDescription('Provides information about a user.')
+		.addUserOption(option =>
+            option.setName('user')
+			.setDescription('The user to get information about.')),
+
 	async execute(interaction) {
-		await interaction.reply(`This command was run by ${interaction.user.username}, who joined this server on ${interaction.member.joinedAt}.`);
+		const user = interaction.options.getMember('user') || interaction.member;
+		const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+		
+		const userDetailsEmbed = new EmbedBuilder()
+		.setColor('Yellow')
+		.setTitle(`${user.user.tag} | User Details`)
+		.setThumbnail(user.displayAvatarURL())
+		.setDescription(`**User tag** \n${user.user.tag} \n \n **User ID** \n${user.user.id} \n \n **User Roles ** \n ${user.roles.cache.map(r => r).join(' | ')} \n \n **Joined Discord** \n${user.user.createdAt.getDate()} ${month[user.user.createdAt.getMonth() + 1]} ${user.user.createdAt.getFullYear()} \n \n **Joined Server** \n${user.joinedAt.getDate()} ${month[user.joinedAt.getMonth() + 1]} ${user.joinedAt.getFullYear()}`)
+
+		await interaction.reply({embeds: [userDetailsEmbed]});
 	},
 };
