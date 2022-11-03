aac560c2142d627102227773d84b135ea81e7189
NehaDevs <cuteybg@gmail.com>
2022-11-03 04:45:22 +0000

    simplified command codes

--- commands/pfp.js
+++ commands/pfp.js
@@ -9,28 +9,13 @@ module.exports = {
                 .setDescription('Person whose profile picture you want to get')),
 
 	async execute(interaction, client) {
-        const user = interaction.options.getUser('user');
-
-        if(!user){
+        const user = interaction.options.getUser('user') || interaction.member;
             const pfpEmbed = new EmbedBuilder()
             .setColor('#FFD700')
             .setTitle('PFP')
             .setDescription('Here is the user\'s profile picture.')
-            .setImage(interaction.member.displayAvatarURL())
+            .setImage(user.displayAvatarURL())
 
-             interaction.reply({embeds: [pfpEmbed]});
+            interaction.reply({embeds: [pfpEmbed]});
         }
-        if(user) {
-            const member = interaction.guild.members.cache.get(user.id) || await interaction.guild.members.fetch(user.id).catch(err => {});
-            if(!member) return interaction.reply({content: 'Can\'t find this user', ephemeral: true})
-
-            const pfpEmbed = new EmbedBuilder()
-            .setColor('#FFD700')
-            .setTitle('PFP')
-            .setDescription('Here is the user\'s profile picture.')
-            .setImage(member.displayAvatarURL())
-
-             interaction.reply({embeds: [pfpEmbed]});
-        }
-	},
-};
+	}
